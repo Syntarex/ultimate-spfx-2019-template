7ccf854f456dabfe426147b5010c331aea778b48
Syntarex <4340303+Syntarex@users.noreply.github.com>
2024-05-05 06:54:54 +0200

    Refactor

--- component/tsup.config.ts
+++ component/tsup.config.ts
@@ -1,13 +1,18 @@
+import fs from "node:fs/promises";
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
     entry: ["src/index.tsx"],
     minify: !options.watch, // Minify in production
     sourcemap: false, // Do not generate source maps. The spfx buildchain will kill them anyway
-    clean: true, // Cleanup build directory
     dts: true, // Generate type definitions of index file
     noExternal: [/(.*)/], // Include used node_modules in bundle
     onSuccess: async () => {
-        console.log("TODO: Shadow safe the index.ts in spfx package");
+        await fs.writeFile(
+            "./../spfx/src/index.ts",
+            "// DO NOT DELETE THIS FILE. This file will be used to trigger the webpack reload of the spfx buildchain if your package changes.",
+        );
+
+        console.info("🚀 Trigger spfx reloading");
     },
 }));
--- spfx/src/index.ts
+++ spfx/src/index.ts
@@ -1,4 +1,1 @@
-/**
- * DO NOT DELETE THIS FILE.
- * This file will be used to trigger the webpack reload of the spfx buildchain if your package changes.
- */
+// DO NOT DELETE THIS FILE. This file will be used to trigger the webpack reload of the spfx buildchain if your package changes.
